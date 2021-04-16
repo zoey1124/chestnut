c0241d5804c897dc1074583dfe480ed7b1c14890
zoey1124 <zoey_1124@berkeley.edu>
2021-04-17 01:57:52 +0800

    add RGA

--- CRDT/CRDT_object.py
+++ CRDT/CRDT_object.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 class GSet(object):
     """An grow-only set object"""
@@ -12,11 +13,17 @@ class GSet(object):
         return self.gset
 
     def merge(self, b):
-        self.gset = self.gset.union(b)
+        """
+        param b: another GSet object
+        return: void, but update local copy of gset
+        """
+        self.gset = self.gset.union(b.gset)
 
-
-class PNSet(object):
-    """A set allows for add and remove. But cannot readd the element after deletion."""
+class USet(object):
+    """
+    A set allows for add and remove.
+    Assume that every element is unique and added only once
+    """
     def __init__(self):
         self.add_set = set()
         self.remove_set = set()
@@ -29,10 +36,10 @@ class PNSet(object):
 
     def remove(self, e):
         self.remove_set.add(e)
-    def merge(self, other_node):
-        self.add_set.union(other_node.add_set)
-        self.remove_set.union(other_node.remove_set)
 
+    def merge(self, other_node):
+        self.add_set = self.add_set.union(other_node.add_set)
+        self.remove_set = self.remove_set.union(other_node.remove_set)
 
 class GCounter(object):
     """Grow-only counter"""
@@ -60,47 +67,183 @@ class PNCounter(object):
         self.neg_counter = np.array([0] * n)
 
     def query(self):
-        return sum(self.pos_counter - self.neg_counter)
+        return sum(self.pos_counter + self.neg_counter)
     def inc(self):
         self.pos_counter[self.nodeId] += 1
     def dec(self):
         self.neg_counter[self.nodeId] -= 1
     def merge(self, b):
         self.pos_counter = np.maximum(self.pos_counter, b.pos_counter)
-        self.neg_counter = np.maximum(self.neg_counter, b.neg_counter)
+        self.neg_counter = np.minimum(self.neg_counter, b.neg_counter)
 
-"""
-What if I want to use CRDT to represent a list o integers?
-Then I can calculate the min, max, average!
-Also, figure out how to use transaction to test!
-"""
-class PNList(object):
+class ListNode(object):
+    def __init__(self, val=None, next=None, deleted=False):
+        self.val = val
+        self.next = next
+        self.deleted = deleted
+
+    def display(self):
+        output = []
+        pointer = self
+        while pointer is not None:
+            if not pointer.deleted:
+                output.append(pointer.val)
+            pointer = pointer.next
+        return str(output)
+
+    def equal(self, other_node):
+        return self.val == other_node.val and self.deleted == other_node.deleted
+
+
+    def deepcopy(self):
+        output = ListNode(0, None, False)
+        self_pt = self
+        out_pt = output
+        while self_pt is not None:
+            out_pt.next = ListNode(self_pt.val, self_pt.next, self_pt.deleted)
+            self_pt = self_pt.next
+            out_pt = out_pt.next
+        return output.next
+
+
+class ItemList(object):
     """
-    CRDT list with fixed length
-    n: number of nodes
-    k: length of the list
+    An ordered list of items (can be String or Integers).
+    Each String has a priority rank, the priority rank can be modified.
+    Only support for unique values for now. Inspired by RGA
     """
-    def __init__(self, n, k, nodeId):
-        self.nodeId = nodeId
-        self.n = n
-        self.k = k
-        self.positive_list = np.zeros((n, k))
-        self.negative_list = np.zeros((n, k))
-        self.list_max = 0
-        self.list_min = 0
-        self.ave = 0
+    def __init__(self):
+        self.items = None
 
-    def query(self):
-        pass
+    def add(self, position, item):
+        """
+        Add item to the position of local copy item list.
+        Position is after item position.
+        """
+        if self.items is None:
+            assert position == 0
+            self.items = item
+        else:
+            head = self.items
+            idx = 0
+            while idx < position - 1 or head.deleted:
+                if not head.deleted:
+                    idx += 1
+                head = head.next
+            after = head.next
+            head.next = item
+            item.next = after
 
-    def merge(self):
+    def lookup(self, item):
+        """Return the position of wish in local copy."""
+        head = self.items
+        position = 0
+        while head is not None:
+            if head.equal(item):
+                return position
+            head = head.next
+            position += 1
+        raise Exception("no such item in the list!")
+
+    def remove(self, position):
+        """
+        Do NOT delete the node form itemlist.
+        Just change node.delete to True
+        """
+        head = self.items
+        while position > 0 or head.deleted:
+            if not head.deleted:
+                position -= 1
+            head = head.next
+        head.deleted = True
+
+    def merge(self, itemlist_copy):
+        """
+        Synchronize (update) local itemList with another node's item list.
+        Return merged itemList
+        """
+        head1 = self.items
+        head2 = itemlist_copy.items
+        merged = ListNode(0)
+        pointer = merged
+        while head1 is not None and head2 is not None:
+            if head1.equal(head2):
+                pointer.next = head1.deepcopy()
+            elif head1.val == head2.val:
+                if head2.deleted:
+                    head1.deleted = True
+                pointer.next = head1.deepcopy()
+            else:
+                smaller = head1.deepcopy() if head1.val < head2.val else head2.deepcopy()
+                bigger = head1.deepcopy() if head1.val > head2.val else head2.deepcopy()
+                pointer.next = smaller
+                pointer = pointer.next
+                pointer.next = bigger
+            head1 = head1.next
+            head2 = head2.next
+            pointer = pointer.next
+
+        # check the rest
+        if head2 is not None:
+            pointer.next = head2.deepcopy()
+
+        self.items = merged.next
+        itemlist_copy.items = merged.next.deepcopy()
+        return merged.next
+
+
+class IntList(ItemList):
+    """
+    Roughly same object as above, except item here should be integer,
+    Support for max, min, ave
+    """
+    def __init__(self):
+        super().__init__()
+        self.minimum = sys.maxsize
+        self.maximum = - sys.maxsize
+        self.summation = 0
+        self.n = 0
+
+    def add(self, position, item):
+        val = item.val
+        self.n += 1
+        self.maximum = max(self.maximum, val)
+        self.minimum = min(self.minimum, val)
+        self.summation += val
+
+        if self.items is None:
+            assert position == 0
+            self.items = item
+        else:
+            head = self.items
+            idx = 0
+            while idx < position - 1 or head.deleted:
+                if not head.deleted:
+                    idx += 1
+                head = head.next
+            after = head.next
+            head.next = item
+            item.next = after
+
+    def remove(self, position):
+        head = self.items
+        while position > 0 or head.deleted:
+            if not head.deleted:
+                position -= 1
+            head = head.next
+        head.deleted = True
+
+        self.n -= 1
+        self.summation -= head.val
+
+    def merge(self, itemlist_copy):
         pass
 
     def list_ave(self):
-        return self.ave
+        return self.summation / float(self.n) if self.n != 0 else 0
 
     def list_max(self):
-        return self.list_max
+        return self.minimum
 
     def list_min(self):
-        return self.list_min
+        return self.maximum
---
+++ CRDT/CRDT_test.py
@@ -0,0 +1,100 @@
+import CRDT_object
+
+def test_GSet():
+    print("===test Gset===")
+    node0 = CRDT_object.GSet()
+    node1 = CRDT_object.GSet()
+    node0.add(2)
+    node1.add(3)
+    node1.add(2)
+    node1.merge(node0)
+    assert node1.gset == {2, 3}
+    assert node0.gset == {2}
+    node0.merge(node1)
+    assert node0.gset == {2, 3}
+    print("Pass")
+
+def test_USet():
+    print("===test USet===")
+    node0 = CRDT_object.USet()
+    node1 = CRDT_object.USet()
+    node0.add(2)
+    node0.add(3)
+    node1.merge(node0)
+    assert node1.query() == {2, 3}
+    node1.remove(2)
+    node1.add(0)
+    node0.merge(node1)
+    assert node0.query() == {0, 3}
+    print("Pass")
+
+def test_PNCounter():
+    print("===test PNCounter===")
+    node0 = CRDT_object.PNCounter(2, 0)
+    node1 = CRDT_object.PNCounter(2, 1)
+    node0.inc()
+    node1.dec()
+    node0.inc()
+    node1.inc()
+    node1.inc()
+    node0.merge(node1)
+    node1.merge(node0)
+    assert node0.query() == node1.query() and node0.query() == 3
+    print("Pass")
+
+def test_ItemList():
+    print("===test ItemList add remove lookup===")
+    node0 = CRDT_object.ItemList()
+    node0.add(0, CRDT_object.ListNode("TV"))
+    assert node0.items.display() == "['TV']"
+    item = CRDT_object.ListNode("TV")
+    assert node0.lookup(item) == 0
+    node0.add(1, CRDT_object.ListNode("Venice"))
+    assert node0.items.display() == "['TV', 'Venice']"
+    node0.add(2, CRDT_object.ListNode("Laptop"))
+    assert node0.items.display() == "['TV', 'Venice', 'Laptop']"
+    node0.remove(1)
+    assert node0.items.display() == "['TV', 'Laptop']"
+    node0.remove(0)
+    assert node0.items.display() == "['Laptop']"
+    node0.add(1, CRDT_object.ListNode("Ski"))
+    assert node0.items.display() == "['Laptop', 'Ski']"
+    node0.add(1, CRDT_object.ListNode("Peace"))
+    assert node0.items.display() == "['Laptop', 'Peace', 'Ski']"
+    print("Pass")
+
+def test_ItemList2():
+    print("===test merge===")
+    node0 = CRDT_object.ItemList()
+    node0.add(0, CRDT_object.ListNode("TV"))
+    node0.add(1, CRDT_object.ListNode("Venice"))
+    node0.add(2, CRDT_object.ListNode("Laptop"))
+
+    node1 = CRDT_object.ItemList()
+    node1.merge(node0)
+    assert node1.items.display() == "['TV', 'Venice', 'Laptop']"
+
+    node0.add(2, CRDT_object.ListNode("Book"))
+    node0.remove(1)
+    node1.add(2, CRDT_object.ListNode("Ski"))
+    assert node0.items.display() == "['TV', 'Book', 'Laptop']"
+    assert node1.items.display() == "['TV', 'Venice', 'Ski', 'Laptop']"
+    node0.merge(node1)
+    assert node0.items.display() == "['TV', 'Book', 'Ski', 'Laptop']"
+
+    node0.add(0, CRDT_object.ListNode("iPhone"))
+    node1.add(0, CRDT_object.ListNode("War"))
+    assert node0.items.display() == "['TV', 'iPhone', 'Book', 'Ski', 'Laptop']"
+    assert node1.items.display() == "['TV', 'War', 'Book', 'Ski', 'Laptop']"
+    node0.merge(node1)
+    assert node0.items.display() == "['TV', 'War', 'iPhone', 'Book', 'Ski', 'Laptop']"
+    assert node1.items.display() == "['TV', 'War', 'iPhone', 'Book', 'Ski', 'Laptop']"
+    print("Pass")
+
+
+if __name__ == "__main__":
+    test_GSet()
+    test_PNCounter()
+    test_ItemList()
+    test_ItemList2()
+    test_USet()
